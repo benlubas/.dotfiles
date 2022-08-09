2b54bb9dc269257667dc7175880791a5a2ed5548
benlubas <benmlubas@gmail.com>
2022-08-09 19:25:49 -0400

    # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #   (use "git restore --staged <file>..." to unstage) # 	new file:   nvim/plugin/git.lua # # Changes not staged for commit: #   (use "git add/rm <file>..." to update what will be committed) #   (use "git restore <file>..." to discard changes in working directory) # 	modified:   .zshrc # 	modified:   README.md # 	modified:   nvim/lua/benlubas/autocomplete.lua # 	modified:   nvim/lua/benlubas/lsp.lua # 	modified:   nvim/lua/benlubas/packer.lua # 	modified:   nvim/lua/benlubas/remap.lua # 	modified:   nvim/lua/benlubas/set.lua # 	modified:   nvim/notes.md # 	modified:   nvim/plugin/marks.lua # 	deleted:    nvim/plugin/neogit.lua # 	modified:   nvim/plugin/packer_compiled.lua # 	modified:   nvim/plugin/telescope.lua # 	modified:   nvim/plugin/visual_improvements.lua # 	modified:   nvim/spell/en.utf-8.add # 	modified:   nvim/spell/en.utf-8.add.spl # 	modified:   symlink.py

---
+++ nvim/plugin/git.lua
@@ -0,0 +1,42 @@
+
+local neogit = require('neogit')
+
+require('gitsigns').setup()
+
+neogit.setup {
+  auto_refresh = true,
+  -- Change the default way of opening neogit
+  kind = "split",
+  -- Change the default way of opening the commit popup
+  commit_popup = {
+    kind = "split",
+  },
+  -- Change the default way of opening popups
+  popup = {
+    kind = "split",
+  },
+  -- Setting any section to `false` will make the section not render at all
+  sections = {
+    untracked = {
+      folded = true
+    },
+    unstaged = {
+      folded = true
+    },
+    staged = {
+      folded = true
+    },
+    stashes = {
+      folded = true
+    },
+    unpulled = {
+      folded = true
+    },
+    unmerged = {
+      folded = true
+    },
+    recent = {
+      folded = true
+    },
+  },
+}
