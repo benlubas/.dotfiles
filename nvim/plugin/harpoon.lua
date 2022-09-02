4b929d49ff2169e10a3fbeb082146a15b12edb9d
benlubas <benmlubas@gmail.com>
2022-09-01 20:52:25 -0400

    switch between nvim's with <leader>j/k

--- nvim/plugin/harpoon.lua
+++ nvim/plugin/harpoon.lua
@@ -14,10 +14,14 @@ nnoremap("<C-j>", function() require("harpoon.ui").nav_file(2) end, silent)
 nnoremap("<C-k>", function() require("harpoon.ui").nav_file(3) end, silent)
 nnoremap("<C-l>", function() require("harpoon.ui").nav_file(4) end, silent)
 
+nnoremap("<leader>j", ":!tmux select-window -t 0<CR><CR>", silent) -- okay, this just works lol.
+nnoremap("<leader>k", ":!tmux select-window -t 2<CR><CR>", silent)
+
 -- tmux navigation.. 
 -- I want to be able to get to the notes window really quickly, and just open my file... are harpoon 
--- binds terminal specific? or if I open another nvim will they be coppied. I kinda don't want them 
--- to get coppied over, that would be a little annoying.
--- Okay, I don't think it likes having two nvim instanses open at the same time... 
+-- binds terminal specific? or if I open another nvim will they be copied. I kinda don't want them 
+-- to get copied over, that would be a little annoying.
+
+-- Okay, I don't think it likes having two nvim instances open at the same time... 
 -- that's a little bit annoying, but honestly... I can probably just jump to notes in this nvim 
 -- if it's so easy to get back.. you know?
