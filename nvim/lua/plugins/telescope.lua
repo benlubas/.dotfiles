579d7ad4f615f29ec7f2fa9e4c34658b401738d8
benlubas <benmlubas@gmail.com>
2023-04-11 13:44:26 -0400

    catching up to main

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -21,17 +21,17 @@ return {
       })
       vim.keymap.set("n", "<leader>fj", require('telescope.builtin').current_buffer_fuzzy_find, {desc = "current buffer fuzzy find" })
       vim.keymap.set("n", "<leader>fa", "<cmd>Telescope live_grep<CR>", {desc = "Find a word in a file in the directory" })
-      vim.keymap.set("n", "<leader>fh", function() require('telescope.builtin').find_files({ hidden = true }) end, { desc = "include hidden files in directory search" })
-      vim.keymap.set("n", "<leader>h", "<cmd>Telescope help_tags<CR>", {desc = "search help tags" })
-      vim.keymap.set("v", "<leader>h", function()
-        require('telescope.builtin').help_tags({ default_text = vim.fn.expand("<cword>") }) end, { desc = "help with visual selction"})
-      vim.keymap.set("n", "<leader>wh",
-      function() require('telescope.builtin').help_tags({ default_text = vim.fn.expand("<cword>") }) end, {desc = "help with word under cursor" })
-      vim.keymap.set("n", "<leader>Wh", function() require('telescope.builtin').help_tags({ default_text = vim.fn.expand("<cWORD>") }) end, { desc = "help with WORD under cursor" })
-      vim.keymap.set("n", "<leader>c", "<cmd>Telescope neoclip<CR>", { desc = "clipboard" })
+      vim.keymap.set("n", "<leader>f.", function() require('telescope.builtin').find_files({ hidden = true }) end, { desc = "include hidden files in directory search" })
+
+      vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", {desc = "search help tags" })
+      vim.keymap.set("v", "<leader>fh", function() require('telescope.builtin').help_tags({ default_text = vim.fn.expand("<cword>") }) end, { desc = "help with visual selction"})
+      vim.keymap.set("n", "<leader>fhw", function() require('telescope.builtin').help_tags({ default_text = vim.fn.expand("<cword>") }) end, {desc = "help with word under cursor" })
+      vim.keymap.set("n", "<leader>fhW", function() require('telescope.builtin').help_tags({ default_text = vim.fn.expand("<cWORD>") }) end, { desc = "help with WORD under cursor" })
+
       vim.keymap.set("n", "<leader>fd", function() require("benlubas.telescope-project-files").project_files() end, { desc = "search project files" })
-      vim.keymap.set("n", "<leader>ss", function() require('telescope.builtin').spell_suggest(require("telescope.themes").get_cursor) end, { desc = "spell suggest" })
-      -- find files in working dir
+      vim.keymap.set("n", "<leader>ss", function() require("telescope.builtin").spell_suggest(require("telescope.themes").get_cursor()) end, { desc = "spell suggest" })
+      vim.keymap.set("n", "<leader>fs", function() require("telescope.builtin").git_status(require("telescope.themes").get_dropdown()) end, { desc = "search modified git files" })
+      vim.keymap.set("n", "<leader><leader>s", ":Telescope symbols<CR>", { desc = "open symbol picker" })
       vim.keymap.set("n", "<leader>fl", function()
         local dir = vim.env.HOME .. "/github/.dotfiles"
         require('telescope.builtin').find_files({
@@ -42,16 +42,5 @@ return {
         function() require("benlubas.telescope-harpoon-mark").harpoon_branch_marks_picker() end,
         { desc = "marks from other branches, select to add marks" })
     end,
-  },
-  { "AckslD/nvim-neoclip.lua", config = true },
-  {
-    "nvim-telescope/telescope-symbols.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim" },
-    config = function()
-      require("telescope.builtin").symbols({ sources = { "emoji", "nerd", "juila" } })
-    end,
-    keys = {
-      { "<leader>fs", "<cmd>telescope symbols<cr>", desc = "open symbol picker" },
-    },
   },
 }
