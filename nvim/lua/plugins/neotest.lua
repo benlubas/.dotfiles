93e77a889554fe01242998b8aee0539fb18e8161
benlubas <benmlubas@gmail.com>
2023-06-14 20:07:12 -0400

    merge main

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,18 +1,19 @@
 return {
-	"nvim-neotest/neotest",
+  "nvim-neotest/neotest",
   keys = {
-    { "<leader>tr", function() require('neotest').run.run() end, desc = "Run the nearest test" },
-    { "<leader>tR", function() require('neotest').run.run(vim.fn.expand("%")) end, desc = "Run the current file" },
-    { "<leader>ts", function() require('neotest').run.stop() end, desc = "stop the nearest test" },
-    { "<leader>ta", function() require('neotest').run.attach() end, desc = "attach to the nearest test" },
+    { "<leader>tr", function() require('neotest').run.run() end,                     desc = "Run the nearest test" },
+    { "<leader>tR", function() require('neotest').run.run(vim.fn.expand("%")) end,   desc = "Run the current file" },
+    { "<leader>ts", function() require('neotest').run.stop() end,                    desc = "stop the nearest test" },
+    { "<leader>ta", function() require('neotest').run.attach() end,                  desc = "attach to the nearest test" },
     { "<leader>th", function() require('neotest').output.open({ enter = true }) end, desc = "open output window" },
     { "<leader>td", function() require('neotest').run.run({ strategy = "dap" }) end, desc = "run the test in debug mode." },
   },
   dependencies = {
     { "haydenmeade/neotest-jest" },
     { "olimorris/neotest-rspec" },
+    { "nvim-neotest/neotest-python" },
   },
-	config = function()
+  config = function()
     require("neotest").setup({
       adapters = {
         -- these are the defaults
@@ -24,6 +25,7 @@ return {
           end,
         }),
         require("neotest-rspec"),
+        require("neotest-python"),
       },
       quickfix = {
         enabled = false,
@@ -55,4 +57,3 @@ return {
     })
   end,
 }
-
