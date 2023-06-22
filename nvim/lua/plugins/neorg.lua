a37022224561bcfc2bf8cb05bd1457eb9569519c
benlubas <benmlubas@gmail.com>
2023-06-22 08:58:55 -0400

    more neorg binds

--- nvim/lua/plugins/neorg.lua
+++ nvim/lua/plugins/neorg.lua
@@ -6,7 +6,12 @@ return {
   lazy = false,
   keys = {
     { "<leader>ni", ":Neorg index<CR>", desc = "Neorg Index", silent = true },
+    { "<leader>nsi", ":e ~/notes/school/index.norg<CR>", desc = "Neorg School Index", silent = true },
+    { "<leader>nwi", ":e ~/notes/work/index.norg<CR>", desc = "Neorg Work Index", silent = true },
+    { "<leader>nww", ":e ~/notes/work/wiki.norg", desc = "Neorg Work Wiki", silent = true },
     { "<leader>nn", ":Neorg keybind norg core.dirman.new.note<CR>", desc = "New Note", silent = true },
+    { "<leader>jt", ":Neorg journal today<CR>", desc = "Journal Today", silent = true },
+    { "<leader>jy", ":Neorg journal yesterday<CR>", desc = "Journal Yesterday", silent = true },
   },
   opts = {
     load = {
@@ -33,6 +38,7 @@ return {
           workspace = "work",
         },
       },
+      ["core.summary"] = {},
       ["core.completion"] = {
         config = {
           engine = "nvim-cmp",
