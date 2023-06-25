99573902d7475fa21635ccbf1a4e40ac6a6bca35
benlubas <benmlubas@gmail.com>
2023-06-24 20:10:43 -0400

    hydra bindings for telescope

---
+++ nvim/lua/plugins/hydra.lua
@@ -0,0 +1,181 @@
+return {
+  "anuvyklack/hydra.nvim",
+  config = function()
+    local hydra = require("hydra")
+
+    local hint = [[
+                 _d_: files          _a_: live grep
+   🭇🬭🬭🬭🬭🬭🬭🬭🬭🬼    _._: all + .files   _j_: current buffer fuzzy find
+  🭉🭁🭠🭘    🭣🭕🭌🬾   _s_: modified files _l_: ~/dotfiles
+  🭅█ ▁     █🭐   _n_: ~/notes        _m_: harpoon marks
+  ██🬿      🭊██   ^
+ 🭤🭒🬺🬹🬱🬭🬭🬭🬭🬵🬹🬹🭝🭙  _f_: resume last search _h_: help
+ 🭋█🬝🮄🮄🮄🮄🮄🮄🮄🮄🬆█🭀  _r_: recent pickers
+ ^
+                 _<Enter>_: Telescope           _<Esc>_
+]]
+
+    local tb = require("telescope.builtin")
+    hydra({
+      name = "Telescope",
+      hint = hint,
+      config = {
+        color = "pink",
+        exit = true,
+        invoke_on_body = true,
+        hint = {
+          position = "middle",
+          border = "rounded",
+        },
+      },
+      mode = "n",
+      body = "<leader>f",
+      heads = {
+        { "d", require("benlubas.telescope.project-files").project_files, { exit = true, nowait = true } },
+        { "a", function() vim.cmd("Telescope live_grep") end,   {  exit = true } },
+        { ".", function() tb.find_files({ hidden = true }) end, {  exit = true } },
+        { "j", tb.current_buffer_fuzzy_find, { exit = true } },
+        { "s", function() tb.git_status(require("telescope.themes").get_dropdown()) end, { exit = true } },
+        { "l",
+          function()
+            local dir = vim.env.HOME .. "/github/.dotfiles"
+            tb.find_files({
+              find_command = { "rg", "--files", "--iglob", "!.git", "--hidden", dir } })
+          end,
+          { exit = true },
+        },
+        { "n",
+          function()
+            local dir = vim.env.HOME .. "/notes"
+            tb.find_files({ find_command = { "rg", "--files", "--iglob", "!.git", "--hidden", dir } })
+          end,
+          { exit = true },
+        },
+        { "m", require("benlubas.telescope.harpoon").harpoon_branch_marks_picker, { exit = true} },
+
+        { "f", tb.resume,                      { exit = true } },
+        { "h", "<cmd>Telescope help_tags<CR>", { exit = true } },
+        { "r", tb.pickers,                     { exit = true } },
+        { "<Enter>", function() vim.cmd("Telescope") end },
+        { "<Esc>",   nil,                            { exit = true, nowait = true } },
+      },
+    })
+
+    local options_hint = [[
+  ^ ^        Options
+  ^
+  _v_ %{ve} virtual edit
+  _i_ %{list} invisible characters
+  _s_ %{spell} spell
+  _w_ %{wrap} wrap
+  _c_ %{cul} cursor line
+  _n_ %{nu} number
+  _r_ %{rnu} relative number
+  ^
+       ^^^^                _<Esc>_
+]]
+
+    hydra({
+      name = "Options",
+      hint = options_hint,
+      config = {
+        color = "amaranth",
+        invoke_on_body = true,
+        hint = {
+          position = "middle",
+          border = "rounded",
+        },
+      },
+      mode = { "n" },
+      body = "<leader><leader>o",
+      heads = {
+        {
+          "n",
+          function()
+            if vim.o.number == true then
+              vim.o.number = false
+            else
+              vim.o.number = true
+            end
+          end,
+        },
+        {
+          "r",
+          function()
+            if vim.o.relativenumber == true then
+              vim.o.relativenumber = false
+            else
+              vim.o.number = true
+              vim.o.relativenumber = true
+            end
+          end,
+        },
+        {
+          "v",
+          function()
+            if vim.o.virtualedit == "all" then
+              vim.o.virtualedit = "block"
+            else
+              vim.o.virtualedit = "all"
+            end
+          end,
+        },
+        {
+          "i",
+          function()
+            if vim.o.list == true then
+              vim.o.list = false
+            else
+              vim.o.list = true
+            end
+          end,
+        },
+        {
+          "s",
+          function()
+            if vim.o.spell == true then
+              vim.o.spell = false
+            else
+              vim.o.spell = true
+            end
+          end,
+        },
+        {
+          "w",
+          function()
+            if vim.o.wrap ~= true then
+              vim.o.wrap = true
+              -- Dealing with word wrap:
+              -- If cursor is inside very long line in the file than wraps
+              -- around several rows on the screen, then 'j' key moves you to
+              -- the next line in the file, but not to the next row on the
+              -- screen under your previous position as in other editors. These
+              -- bindings fixes this.
+              vim.keymap.set("n", "k", function()
+                return vim.v.count > 0 and "k" or "gk"
+              end, { expr = true, desc = "k or gk" })
+              vim.keymap.set("n", "j", function()
+                return vim.v.count > 0 and "j" or "gj"
+              end, { expr = true, desc = "j or gj" })
+            else
+              vim.o.wrap = false
+              vim.keymap.del("n", "k")
+              vim.keymap.del("n", "j")
+            end
+          end,
+        },
+        {
+          "c",
+          function()
+            if vim.o.cursorline == true then
+              vim.o.cursorline = false
+            else
+              vim.o.cursorline = true
+            end
+          end,
+        },
+        { "<Esc>", nil, { exit = true } },
+      },
+    })
+  end,
+}
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,3 +1,5 @@
+-- NOTE: I have a lot of telescope mappings setup as "Hydras" see nvim/lua/plugins/hydra.lua
+
 return {
   {
     "nvim-telescope/telescope.nvim",
@@ -8,15 +10,15 @@ return {
     config = function()
       require("telescope").setup({
         defaults = {
-          layout_strategy = 'vertical',
-          layout_config = {
-            vertical = {
-              mirror = true,
-            },
-          },
+          layout_strategy = "center",
           path_display = { shorten = { len = 3, exclude = { 1, 2, -1, -2 }}},
           dynamic_preview_title = true,
           winblend = 13,
+          sorting_strategy = "ascending",
+          cache_picker = {
+            num_pickers = 15, -- default 1
+            limit_entries = 500, -- default 500
+          },
           mappings = {
             i = {
               ["<esc>"] = require("telescope.actions").close,
@@ -31,48 +33,14 @@ return {
         },
         pickers = {
           live_grep = {
-            cache_picker = {
-              num_pickers = 15, -- default 1
-              limit_entries = 500, -- default 500
-            },
             mappings = {
               i = { ["<c-f>"] = require("telescope.actions").to_fuzzy_refine },
             },
           },
         },
       })
-      vim.keymap.set("n", "<leader>ff", require("telescope.builtin").resume, { desc = "resume last search" })
-
-      vim.keymap.set("n", "<leader>fj", require("telescope.builtin").current_buffer_fuzzy_find, { desc = "current buffer fuzzy find" })
-      vim.keymap.set("n", "<leader>fa", "<cmd>Telescope live_grep<CR>", { desc = "Find a word in a file in the directory" })
-      vim.keymap.set("n", "<leader>f.", function() require("telescope.builtin").find_files({ hidden = true }) end, { desc = "include hidden files in directory search" })
-
-      vim.keymap.set("n", "<leader>fhf", "<cmd>Telescope help_tags<CR>", { desc = "search help tags" })
-      vim.keymap.set("v", "<leader>fh", function() require("telescope.builtin").help_tags({ default_text = vim.fn.expand("<cword>") }) end, { desc = "help with visual selection" })
-      vim.keymap.set("n", "<leader>fhw", function() require("telescope.builtin").help_tags({ default_text = vim.fn.expand("<cword>") }) end, { desc = "help with word under cursor" })
-      vim.keymap.set("n", "<leader>fhW", function() require("telescope.builtin").help_tags({ default_text = vim.fn.expand("<cWORD>") }) end, { desc = "help with WORD under cursor" })
-
-      vim.keymap.set("n", "<leader>fd", function() require("benlubas.telescope.project-files").project_files() end, { desc = "search project files" })
       vim.keymap.set("n", "<leader>ss", function() require("telescope.builtin").spell_suggest(require("telescope.themes").get_cursor()) end, { desc = "spell suggest" })
-      vim.keymap.set("n", "<leader>fs", function() require("telescope.builtin").git_status(require("telescope.themes").get_dropdown()) end, { desc = "search modified git files" })
       vim.keymap.set("n", "<leader><leader>s", ":Telescope symbols<CR>", { desc = "open symbol picker" })
-      vim.keymap.set("n", "<leader>fl", function()
-        local dir = vim.env.HOME .. "/github/.dotfiles"
-        require("telescope.builtin").find_files({
-          find_command = { "rg", "--files", "--iglob", "!.git", "--hidden", dir },
-        })
-      end, { desc = "search dot files" })
-
-    vim.keymap.set("n", "<leader>fn", function()
-        local dir = vim.env.HOME .. "/notes"
-        require("telescope.builtin").find_files({
-          find_command = { "rg", "--files", "--iglob", "!.git", "--hidden", dir },
-        })
-      end, { desc = "search in notes directory" })
-
-      vim.keymap.set("n", "<leader>fm",
-        function() require("benlubas.telescope.harpoon").harpoon_branch_marks_picker() end,
-        { desc = "marks from other branches, select to add marks" })
       vim.keymap.set("n", "<leader>mx",
         function() require("benlubas.telescope.tmux").tmux_sessions_picker({ "~/github", "~/work", "~/notes" }) end,
         { desc = "tmux-sessionizer but telescope" })
