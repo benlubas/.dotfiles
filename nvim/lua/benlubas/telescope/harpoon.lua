1cc65c1523cf472cdcd06b368bec16a02250c7f7
benlubas <benmlubas@gmail.com>
2023-06-30 09:05:26 -0400

    small todo item

--- nvim/lua/benlubas/telescope/harpoon.lua
+++ nvim/lua/benlubas/telescope/harpoon.lua
@@ -14,6 +14,9 @@ M.mark_file = function(tb)
   actions.drop_all(tb)
   actions.add_selection(tb)
   telescope_utils.map_selections(tb, function(selection)
+    -- TODO: This doesn't work for the git status picker, or the live grep picker. The formatting
+    -- there is different, but consistent and detectable, so we should be fine there
+    -- See: P(selection)
     local file = selection[1]
     pcall(require("harpoon.mark").add_file, file)
   end)
