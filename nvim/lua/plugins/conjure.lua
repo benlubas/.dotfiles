bb4d758bcbac382c6b44e8d5c30951e617526143
benlubas <benmlubas@gmail.com>
2023-08-12 20:00:11 -0400

    conjure

---
+++ nvim/lua/plugins/conjure.lua
@@ -0,0 +1,9 @@
+
+return {
+  "Olical/conjure",
+  setup = function ()
+    -- this plugin is configured with global variables, :h conjure-config to see them
+
+    -- local leader is `\` which is sym + c on my keyboard
+  end,
+}
