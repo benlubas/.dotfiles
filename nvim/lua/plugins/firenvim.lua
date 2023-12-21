46c2c331c138879eacb6cb763b6da09fac4fc81f
benlubas <benmlubas@gmail.com>
2023-12-21 13:29:05 -0500

    never auto takeover with firenvim

--- nvim/lua/plugins/firenvim.lua
+++ nvim/lua/plugins/firenvim.lua
@@ -9,32 +9,17 @@ return {
     vim.fn["firenvim#install"](0)
   end,
   init = function()
-    local never_open = {
-      [[.*notion\.so.*]],
-      [[.*docs\.google\.com.*]],
-      [[.*teams\.microsoft\.com.*]],
-      [[.*gradescope\.com.*]],
-      [[.*web\.whatsapp\.com.*]],
-      [[.*messages\.google\.com.*]],
-    }
-    local localSettings = {
-      [".*"] = {
-        cmdline = "firenvim",
-        content = "text",
-        priority = 0,
-        selector = 'textarea:not([readonly]):not([class="handsontableInput"]), div[role="textbox"]',
-        takeover = "empty",
-      },
-    }
-    for _, v in ipairs(never_open) do
-      localSettings[v] = {
-        priority = 9,
-        takeover = "never",
-      }
-    end
     vim.g.firenvim_config = {
       globalSettings = { alt = "all" },
-      localSettings = localSettings,
+      localSettings = {
+        [".*"] = {
+          cmdline = "firenvim",
+          content = "text",
+          priority = 0,
+          selector = 'textarea:not([readonly]):not([class="handsontableInput"]), div[role="textbox"]',
+          takeover = "never",
+        },
+      },
     }
   end,
 }
