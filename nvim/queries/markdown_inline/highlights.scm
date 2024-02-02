9197ff57f339f0257786f81c9483accf2d7c4586
benlubas <benmlubas@gmail.com>
2024-02-01 20:50:04 -0500

    markdown quiries

---
+++ nvim/queries/markdown_inline/highlights.scm
@@ -0,0 +1,14 @@
+((shortcut_link
+   ((link_text) @text
+                (#eq? @text "!WARNING"))
+   ) @text.warning (#set! conceal ""))
+
+((shortcut_link
+   ((link_text) @text
+                (#eq? @text "!INFO"))
+   ) @text.title.help (#set! conceal ""))
+
+((shortcut_link
+   ((link_text) @text
+                (#eq? @text "!IMPORTANT"))
+   ) @text.title (#set! conceal ""))
