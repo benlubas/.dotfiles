08b1b9a41a434eea9d8f36432a1e79a7c7d90c63
Ben Lubas <benmlubas@gmail.com>
2024-06-01 10:56:57 -0400

    Neorg snippets

--- nvim/lua/snippets/norg.lua
+++ nvim/lua/snippets/norg.lua
@@ -2,18 +2,33 @@ local ls = require("luasnip")
 
 local s = ls.snippet
 local i = ls.insert_node
--- local t = ls.text_node
+local t = ls.text_node
 -- local d = ls.dynamic_node
 -- local c = ls.choice_node
--- local f = ls.function_node
+local f = ls.function_node
 -- local r = ls.restore_node
-local fmt = require("luasnip.extras.fmt").fmta
+local fmta = require("luasnip.extras.fmt").fmta
 -- local h = require("snippets.snippet_helpers")
+
+local function swap(pattern)
+  return s(
+    {
+      trig = pattern,
+      trigEngine = "pattern",
+      hidden = true,
+    },
+    fmta("<one><two><last>", {
+      one = f(function(_, parent) return parent.snippet.captures[2] end),
+      two = f(function(_, parent) return parent.snippet.captures[1] end),
+      last = i(0),
+    })
+  )
+end
 
 ls.add_snippets("norg", {
   s(
     "code",
-    fmt(
+    fmta(
       [[@code <lang>
 <last>
 @end]],
@@ -23,4 +38,6 @@ ls.add_snippets("norg", {
       }
     )
   ),
+  swap("({.*})(%[.*%])"),
+  swap("(%[.*%])({.*})"),
 })
